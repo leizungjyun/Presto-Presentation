8c0834858c935a6a11808cf8c8a46e40e56fb8db
LI Shaun <leizungjyun@zju.edu.cn>
2023-11-08 17:07:23 +0800

    refactor xl2md.py

--- xl2md.py
+++ xl2md.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
-
-# %%
-# openpyxl requied
-import pandas as pd
+import pandas as pd # openpyxl requied
 import os.path
+import sys
 
-# %%
 # use the second row as the column name
 # ignore the third and forth row
 # pandas's read_excel is based on openpyxl
 # the input xlsx is specified by command line argument
-
-import sys
 if len(sys.argv) > 1:
     # decide if file exists
     if not os.path.isfile(sys.argv[1]):
@@ -23,13 +18,12 @@ else:
     # print error message
     print("Usage: python xl2md.py <input xlsx>")
     print("Example: python xl2md.py 12.17音乐会节目信息征集.xlsx")
-    # exit the program
     sys.exit(1)
 
+# create a new markdown file named program prensentation which begins with the `front_matter`
+# write to the file specified by the second command line argument
+md_file = "slides.md"
 
-
-
-# %%
 # define a markdown front matter
 front_matter="""---
 marp: true
@@ -38,41 +32,17 @@ class: invert
 ---
 
 <style>
-  table {
+table {
     font-size: 30px;
-  }
-  th,td {
+}
+th,td {
     border: none!important;
-  }
-  section {
+}
+section {
     font-family: "Garamond";
-  }
+}
 </style>
 """
-
-# create a new markdown file named program prensentation which begins with the `front_matter`
-# write to the file specified by the second command line argument
-
-
-md_file = "slides.md"
-
-
-with open(md_file, 'w', encoding='utf-8') as f:
-    f.write(front_matter)
-
-
-
-# %%
-# the first page of the program presentation 
-frontpage = """# 时光流转 琴音不辍
-浙江大学研究生艺术团钢琴音乐会
-"""
-
-# write the frontpage
-with open(md_file, 'a', encoding='utf-8') as f:
-    f.write(frontpage)
-    f.write('\n---\n')
-
 # the second page is warping
 warping = """# 注意事项
 1. 请关闭您的手机或将您的手机调至静音状态
@@ -80,33 +50,31 @@ warping = """# 注意事项
 1. 请妥善放置塑料袋等容易发出声响的物品
 1. 在演出期间，请您尽量避免在场内来回走动
 """
+# the first page of the program presentation 
+frontpage = """# 时光流转 琴音不辍
+浙江大学研究生艺术团钢琴音乐会
+"""
 
-# write the warping page
-with open(md_file, 'a', encoding='utf-8') as f:
+with open(md_file, 'w', encoding='utf-8') as f:
+    f.write(front_matter)
+    f.write(frontpage)
+    f.write('\n---\n')
     f.write(warping)
     f.write('\n---\n')
-
-# %%
-# for each row in df, conver it and write to the markdown file as follows
-# 曲目中文名 --> ### 曲目中文名
-# 曲目英文名 --> **曲目英文名**
-# 作曲家中文 作曲家英文 --> ##### 作曲家中文 作曲家英文
-# 表演者 表演者拼音 --->
-# |       |      |
-# | :-----|:------|
-# | 表演者 | 表演者 |
-# | Performer | 表演者拼音 |
-# and end with --- except the last row
-
-with open(md_file, 'a', encoding='utf-8') as f:
+    # for each row in df, conver it and write to the markdown file as follows
+    # 曲目中文名 --> ### 曲目中文名
+    # 曲目英文名 --> **曲目英文名**
+    # 作曲家中文 作曲家英文 --> ##### 作曲家中文 作曲家英文
+    # 表演者 表演者拼音 --->
+    # |       |      |
+    # | :-----|:------|
+    # | 表演者 | 表演者 |
+    # | Performer | 表演者拼音 |
+    # and end with --- except the last row
     for index, row in df.iterrows():
-
         if row["曲目中文名"] == "中场休息":
-            
             f.write(f'# {row["曲目中文名"]}\n')
             f.write(f'10分钟\n')
-
-
         else:
             f.write(f'### {row["曲目中文名"]}\n')
             f.write(f'**{row["曲目英文名"]}**\n')
@@ -120,13 +88,9 @@ with open(md_file, 'a', encoding='utf-8') as f:
             f.write(f'| :-----|:------|\n')
             f.write(f'| 表演者 | {row["表演者中文"]} |\n')
             f.write(f'| Performer | {row["表演者英文"]} |\n')
-
         f.write('\n---\n')
 
-
-# %%
-# the last page is the same as the front page
-with open(md_file, 'a', encoding='utf-8') as f:
+    # the last page is the same as the front page
     f.write(frontpage)
 
 # print out successful message
